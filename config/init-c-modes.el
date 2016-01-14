72bc299b2190016605ea58077995051a4cf4c16d
Erik Lundmark <cjiqqq@gmail.com>
2016-01-14 22:21:50 +0100

    Add c-modes settings.

---
+++ config/init-c-modes.el
@@ -0,0 +1,9 @@
+;; code goes here
+(require 'cc-mode)
+(setq c-default-style "linux"
+      c-basic-offset 2)
+(setq-default c-basic-offset 2
+              tab-width 2
+              indent-tabs-mode nil)
+(define-key c-mode-base-map (kbd "RET") 'newline-and-indent)
+(add-to-list 'auto-mode-alist '("\\.hps\\" . c++-mode))
